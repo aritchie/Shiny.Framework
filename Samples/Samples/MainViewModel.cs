b2fb1a4cad02777ef24eb1008998c7e543ef4c04
Allan Ritchie <allan.ritchie@gmail.com>
2021-05-16 15:08:46 -0400

    WIP

--- Samples/Samples/MainViewModel.cs
+++ Samples/Samples/MainViewModel.cs
@@ -1,14 +1,20 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Input;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using Shiny;
 using Shiny.Msal;
+using Xamarin.Forms;
+
 
 namespace Samples
 {
     public class MainViewModel : ViewModel
     {
-        public MainViewModel(IDialogs dialogs, IMsalService msal)
+        public MainViewModel(IDialogs dialogs)
         {
+            IMsalService msal = null;
             this.Title = "Framework";
             this.Button = ReactiveCommand.CreateFromTask(async () => await dialogs.Alert("HI"));
 
@@ -29,6 +35,23 @@ namespace Samples
                 await msal.TryRefresh();
                 await dialogs.Snackbar("Refresh success");
             });
+
+            this.WhenAnyValue(x => x.Text1IsFocused)
+                .Select(focused => focused ? Color.Red : Color.Black)
+                .Subscribe(color => {
+                    this.Text1Color = color;
+                })
+                .DisposedBy(this.DestroyWith);
+
+            this.WhenAnyValue(x => x.Text2IsFocused)
+                .Select(focused => focused ? Color.Red : Color.Black)
+                .Subscribe(color => {
+                    this.Text2Color = color;
+                })
+                .DisposedBy(this.DestroyWith);
+
+            this.Toggle1 = ReactiveCommand.Create(() => this.Text1IsFocused = !this.Text1IsFocused);
+            this.Toggle2 = ReactiveCommand.Create(() => this.Text2IsFocused = !this.Text2IsFocused);
         }
 
 
@@ -36,5 +59,15 @@ namespace Samples
         public ICommand MsalSignIn { get; }
         public ICommand MsalSignOut { get; }
         public ICommand MsalRefresh { get; }
+
+        public ICommand Toggle1 { get; }
+        [Reactive] public string Text1 { get; set; }
+        [Reactive] public bool Text1IsFocused { get; set; }
+        [Reactive] public Color Text1Color { get; set; }
+
+        public ICommand Toggle2 { get; }
+        [Reactive] public string Text2 { get; set; }
+        [Reactive] public bool Text2IsFocused { get; set; }
+        [Reactive] public Color Text2Color { get; set; }
     }
 }
---
+++ src/Shiny.Framework/XamForms/EntryFocusBindBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+
+namespace Shiny.XamForms
+{
+    public class EntryFocusBindBehavior : Behavior<Entry>
+    {
+        Entry? boundEntry;
+
+
+        public static readonly BindableProperty IsFocusedProperty = BindableProperty.Create(
+            nameof(IsFocused),
+            typeof(bool),
+            typeof(EntryFocusBindBehavior),
+            false,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: OnFocusChanged
+        );
+        public bool IsFocused
+        {
+            get => (bool)this.GetValue(IsFocusedProperty);
+            set => this.SetValue(IsFocusedProperty, value);
+        }
+
+
+        static void OnFocusChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var behave = (EntryFocusBindBehavior)bindable;
+            behave.SetFocus(behave.boundEntry, (bool)newValue);
+        }
+
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            // TODO: not triggering unfocus back to bind
+            base.OnAttachedTo(bindable);
+            this.boundEntry = bindable;
+            bindable.Focused += this.OnFocused;
+            bindable.Unfocused += this.OnFocused;
+        }
+
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.boundEntry = null;
+            bindable.Focused -= this.OnFocused;
+            bindable.Unfocused -= this.OnFocused;
+        }
+
+
+        void OnFocused(object sender, FocusEventArgs e)
+            => this.IsFocused = e.IsFocused;
+
+
+        void SetFocus(Entry? entry, bool flag)
+        {
+            if (entry == null)
+                return;
+
+            if (flag)
+                entry.Focus();
+            else
+                entry.Unfocus();
+        }
+    }
+}
